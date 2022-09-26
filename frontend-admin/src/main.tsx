bd4496425a83435017828a7219578eb079824098
kara <ssctonioloriccardo@gmail.com>
2022-09-26 19:14:20 +0200

    Implemented the custom component and the logic in Redux index..

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -4,17 +4,20 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Header from './components/header/header';
+import IsLoggedIn from './components/utility/isLoggedIn';
 import './index.css';
 import store from './store/index';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <Header>
-                    <App />
-                </Header>
-            </BrowserRouter>
+            <IsLoggedIn>
+                <BrowserRouter>
+                    <Header>
+                        <App />
+                    </Header>
+                </BrowserRouter>
+            </IsLoggedIn>
         </Provider>
     </React.StrictMode>
 );
--- frontend-admin/src/store/index.ts
+++ frontend-admin/src/store/index.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import orderSlice from './order-slice';
+import loginSlice from './login-slice';
 
 const store = configureStore({
-    reducer: { order: orderSlice.reducer },
+    reducer: { order: orderSlice.reducer, login: loginSlice.reducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
