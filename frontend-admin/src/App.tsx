3fb95ded8bdc44fb7f24b5814ab16f80c8cdbe5f
kara <ssctonioloriccardo@gmail.com>
2022-09-30 19:32:12 +0200

    Added lazy loading to reduce the bundle size, depending on the usage of the app.

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,41 +1,61 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Suspense } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import AddFood from './pages/addFood';
-import AddIngredient from './pages/addIngredient';
-import AddLiquid from './pages/addLiquid';
-import AddQuantity from './pages/addQuantity';
-import ConfirmOrderId from './pages/confirmOrderId';
-import Login from './pages/login';
-import Order from './pages/order';
-import PrintOrderId from './pages/printOrderId';
-import SearchComponent from './pages/searchComponent';
+
+const AddFood = React.lazy(() => import('./pages/addFood'));
+const AddIngredient = React.lazy(() => import('./pages/addIngredient'));
+const AddLiquid = React.lazy(() => import('./pages/addLiquid'));
+const AddQuantity = React.lazy(() => import('./pages/addQuantity'));
+const ConfirmOrderId = React.lazy(() => import('./pages/confirmOrderId'));
+const Delete = React.lazy(() => import('./pages/delete'));
+const Login = React.lazy(() => import('./pages/login'));
+const Order = React.lazy(() => import('./pages/order'));
+const PrintOrderId = React.lazy(() => import('./pages/printOrderId'));
+const SearchComponent = React.lazy(() => import('./pages/searchComponent'));
+
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Navigate to="/order" replace />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/order" element={<Order />} />
-                <Route
-                    path="/confirm"
-                    element={
-                        <SearchComponent title="Inserisci l'id dell'ordine da confermare:" />
-                    }
-                />
-                <Route path="/confirm/:orderId" element={<ConfirmOrderId />} />
-                <Route
-                    path="/print"
-                    element={
-                        <SearchComponent title="Inserisci l'id dell'ordine da stampare:" />
-                    }
-                />
-                <Route path="/add-ingredient" element={<AddIngredient />} />
-                <Route path="/add-liquid" element={<AddLiquid />} />
-                <Route path="/add-quantity" element={<AddQuantity />} />
-                <Route path="/add-food" element={<AddFood />} />
-                <Route path="/print/:orderId" element={<PrintOrderId />} />
-                <Route path="*" element={<h1>Pagina non trovata.</h1>} />
-            </Routes>
+            <Suspense
+                fallback={
+                    <div>
+                        <Spinner role="status" animation="border" />
+                    </div>
+                }
+            >
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate to="/order" replace />}
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/order" element={<Order />} />
+                    <Route
+                        path="/confirm"
+                        element={
+                            <SearchComponent title="Inserisci l'id dell'ordine da confermare:" />
+                        }
+                    />
+                    <Route
+                        path="/confirm/:orderId"
+                        element={<ConfirmOrderId />}
+                    />
+                    <Route
+                        path="/print"
+                        element={
+                            <SearchComponent title="Inserisci l'id dell'ordine da stampare:" />
+                        }
+                    />
+                    <Route path="/add-ingredient" element={<AddIngredient />} />
+                    <Route path="/add-liquid" element={<AddLiquid />} />
+                    <Route path="/add-quantity" element={<AddQuantity />} />
+                    <Route path="/add-food" element={<AddFood />} />
+                    <Route path="/delete" element={<Delete />} />
+                    <Route path="/print/:orderId" element={<PrintOrderId />} />
+                    <Route path="*" element={<h1>Pagina non trovata.</h1>} />
+                </Routes>
+            </Suspense>
         </>
     );
 }
